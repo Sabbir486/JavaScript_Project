496efe9cd033feadea3028998e93c4ebc8652cb3
Sabbir486 <australiasabbir355@gmail.com>
2025-08-18 03:43:39 +0600

    Add

---
+++ Project_01/index.js
@@ -0,0 +1,30 @@
+const btn = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+btn.forEach(function(button) {
+    console.log(button);
+
+    // Add Event Listener
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = 'grey';
+        }
+
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = 'white';
+        }
+
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = 'blue';
+        }
+
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = 'yellow';
+        }
+
+    })
+    
+});
